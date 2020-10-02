00ae3c0508798d6c3e5c5c807ae60d8b4992e77f
senay berhe <tsionberhe@gmail.com>
2020-10-02 16:51:27 -0700

    created templates and views

---
+++ account/forms.py
@@ -0,0 +1,19 @@
+from django import forms 
+from .models import Profile 
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    
+
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User 
+        fields = ('first_name', 'last_name', 'email')
+
+
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('date_of_birth', 'photo')
